bad13942b3dd8414598b75e687b02f34462f98b0
zebokurbanova <zebokurbanova220@gmail.com>
2025-05-01 19:18:28 +0500

    Add files via upload
    Python

---
+++ hello.py
@@ -0,0 +1,25 @@
+a= int(input(" 1da 7gacha bolgan raqamni kiriting a="))
+
+if( a==1):
+print("dushanba")
+
+elif(a==2):
+print("seshanba")
+
+elif(a==3):
+print("chorshanba")
+
+elif(a==4):
+print("payshanba")
+
+elif(a==5):
+print("Juma")
+
+elif(a==6):
+print("shanba")
+
+elif(a==7):
+print("yakshanba")
+
+else:
+    print("1 dan 7 gacha bolagan raqamni kiriting")
